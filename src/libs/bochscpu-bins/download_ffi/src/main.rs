918f74eeddd6628453af93736b1792b9ce4391a8
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2026-08-23 11:17:41 -0700

    download_ffi

---
+++ src/libs/bochscpu-bins/download_ffi/src/main.rs
@@ -0,0 +1,189 @@
+// Axel '0vercl0k' Souchet - August 20 2026
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::Write;
+use std::ops::{Deref, DerefMut};
+use std::path::{Path, PathBuf};
+use std::sync::LazyLock;
+use std::{env, io};
+
+use reqwest::blocking::ClientBuilder;
+use serde_json::Value;
+use zip::ZipArchive;
+
+struct StaticLibInfo {
+    /// Path to the static library file that needs to be extracted from the zip
+    /// archive (like `release/bochscpu_ffi.lib`)..
+    path_in_zip: &'static str,
+    /// ..and renamed as `new_filename` (like `bochscpu_ffi_Windows_AMD64.lib`).
+    new_filename: &'static str,
+}
+
+impl StaticLibInfo {
+    const fn new(path_in_zip: &'static str, new_filename: &'static str) -> Self {
+        Self {
+            path_in_zip,
+            new_filename,
+        }
+    }
+}
+
+/// This is the API that gives us the zip URLs for the latest releases made in
+/// the `bochscpu-ffi` repo.
+const BXCPUFFI_LATEST_RELEASE_LINK: &str =
+    "https://api.github.com/repos/yrp604/bochscpu-ffi/releases/latest";
+
+static BXCPUFFI_RELEASE_ZIP_FILENAMES: LazyLock<HashMap<&'static str, StaticLibInfo>> =
+    LazyLock::new(|| {
+        macro_rules! staticlibs {
+            ($( $zip_name:literal => ($staticlib_path:literal, $filename:literal) )+) => {{
+                let mut h = HashMap::new();
+
+                $(
+                    assert!(
+                        h.insert($zip_name, StaticLibInfo::new($staticlib_path, $filename))
+                        .is_none()
+                    );
+                )+
+
+                h
+            }}
+        }
+
+        staticlibs!(
+            "bochscpu-ffi-windows-latest-x64-MT.zip" => (
+                "release/bochscpu_ffi.lib",
+                "bochscpu_ffi_Windows_AMD64.lib"
+            )
+            "bochscpu-ffi-ubuntu-latest-x64.zip" => (
+                "release/libbochscpu_ffi.a",
+                "libbochscpu_ffi_Linux_x86_64.a"
+            )
+            "bochscpu-ffi-ubuntu-24.04-arm-arm64.zip" => (
+                "release/libbochscpu_ffi.a",
+                "libbochscpu_ffi_Linux_aarch64.a"
+            )
+            "bochscpu-ffi-macos-latest-arm64.zip" => (
+                "release/libbochscpu_ffi.a",
+                "libbochscpu_ffi_Darwin_arm64.a"
+            )
+        )
+    });
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+/// Get the URLs to the bochscpu-ffi zip files that contains the static library
+/// `wtf` needs for linking.
+fn fetch_bxcpuffi_zip_assets_urls() -> Result<(String, HashMap<String, String>)> {
+    let cli = ClientBuilder::new()
+        .user_agent("Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/15")
+        .build()?;
+    let res = cli.get(BXCPUFFI_LATEST_RELEASE_LINK).send()?.text()?;
+    let js: Value = serde_json::from_str(&res)?;
+    let assets = js["assets"].as_array().ok_or("no assets?")?;
+    let tag_name = js["tag_name"].as_str().ok_or("no tag name?")?.to_string();
+
+    let zip_urls_by_name = assets
+        .iter()
+        .map(|e| {
+            (
+                e["name"].as_str().unwrap().to_string(),
+                e["browser_download_url"].as_str().unwrap().to_string(),
+            )
+        })
+        .collect();
+
+    Ok((tag_name, zip_urls_by_name))
+}
+
+struct ScopedTempFilePath(PathBuf);
+
+impl Drop for ScopedTempFilePath {
+    fn drop(&mut self) {
+        println!("Cleaning up {}", self.0.display());
+        fs::remove_file(&self.0).unwrap();
+    }
+}
+
+impl From<PathBuf> for ScopedTempFilePath {
+    fn from(value: PathBuf) -> Self {
+        Self(value)
+    }
+}
+
+impl AsRef<Path> for ScopedTempFilePath {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
+
+struct ScopedTempFile {
+    file: File,
+    _path: ScopedTempFilePath,
+}
+
+impl ScopedTempFile {
+    fn new(filename: impl AsRef<Path>) -> Result<Self> {
+        let path = env::temp_dir().join(filename.as_ref()).into();
+        let file = File::options()
+            .truncate(true)
+            .write(true)
+            .read(true)
+            .open(&path)?;
+
+        Ok(Self { file, _path: path })
+    }
+}
+
+impl Deref for ScopedTempFile {
+    type Target = File;
+
+    fn deref(&self) -> &Self::Target {
+        &self.file
+    }
+}
+
+impl DerefMut for ScopedTempFile {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.file
+    }
+}
+
+fn main() -> Result<()> {
+    println!("Getting the latest bochscpu-ffi assets URLs..");
+    let (tag_name, mut zip_urls) = fetch_bxcpuffi_zip_assets_urls()?;
+    for (zip_filename, staticlib_info) in BXCPUFFI_RELEASE_ZIP_FILENAMES.iter() {
+        let zip_url = zip_urls.remove(*zip_filename).ok_or("entry is missing")?;
+        let downloaded_zip_path = env::temp_dir().join(zip_filename);
+        println!(
+            "Downloading {zip_url} to {}..",
+            downloaded_zip_path.display()
+        );
+
+        let mut downloaded_zip = ScopedTempFile::new(downloaded_zip_path)?;
+        io::copy(&mut reqwest::blocking::get(zip_url)?, &mut *downloaded_zip)?;
+
+        let staticlib_path = format!("../lib/{}", staticlib_info.new_filename);
+        let mut staticlib = File::create(&staticlib_path)?;
+        let mut zip = ZipArchive::new(&*downloaded_zip)?;
+        println!(
+            "Extracting {} into {staticlib_path}..",
+            staticlib_info.path_in_zip
+        );
+
+        io::copy(
+            &mut zip.by_name(staticlib_info.path_in_zip)?,
+            &mut staticlib,
+        )?;
+    }
+
+    // The only zip we don't 'consume' the windows-MD zip.
+    assert_eq!(zip_urls.len(), 1);
+    println!("Extracted all the files!");
+
+    let mut version = File::create("../TAG")?;
+    write!(version, "{tag_name}")?;
+
+    Ok(())
+}
